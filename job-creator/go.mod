9d96bf45a704f316cae62f1ae13547013e519c90
Sean Connole <sean.connole@dmetrics.com>
2025-07-30 22:23:57 -0500

    feat: add job creator

---
+++ job-creator/go.mod
@@ -0,0 +1,7 @@
+module go-services/job-creator
+
+go 1.23.0
+
+require github.com/bxcodec/faker/v4 v4.0.0-beta.3
+
+require golang.org/x/text v0.3.7 // indirect
---
+++ job-creator/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/bxcodec/faker/v4"
+)
+
+type Job struct {
+	Data string `json:"data"`
+}
+
+func main() {
+	fakeSentence := faker.Sentence()
+
+	job := Job{Data: fakeSentence}
+	jsonData, err := json.Marshal(job)
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := http.Post("http://api.default.svc.cluster.local/jobs", "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	log.Printf("Sent Job: %s\nResponseStatus: %s\n", fakeSentence, resp.Status)
+}
